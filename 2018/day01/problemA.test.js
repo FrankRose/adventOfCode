f96dfc443cd5e50be07dc03723711685f0edefd7
Frank Rose <franklin.rose@gmail.com>
2018-12-01 17:56:01 -0500

    Added all sample tests for problem A of day 01

--- 2018/day01/problemA.test.js
+++ 2018/day01/problemA.test.js
@@ -3,3 +3,11 @@ const { problemA } = require('./problemA');
 test('correctly solves sample problem #1', () => {
   expect(problemA(['+1', '+1', '+1'])).toBe(3);
 });
+
+test('correctly solves sample problem #2', () => {
+  expect(problemA(['+1', '+1', '-2'])).toBe(0);
+});
+
+test('correctly solves sample problem #3 ([-1, -2, -3])', () => {
+  expect(problemA(['-1', '-2', '-3'])).toBe(-6);
+});
