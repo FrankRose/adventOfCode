941760a788eaff091eebaab5eb04819688297748
Frank Rose <franklin.rose@gmail.com>
2018-12-03 16:48:42 -0500

    Solved problem A of day 03

--- 2018/day03/partA.js
+++ 2018/day03/partA.js
@@ -1,7 +1,51 @@
+const path = require('path');
 const readInputFile = require('../utils/readInputFile');
 
-const problemA = input => {
-  throw new Error('Not Implemented');
+const getRectangles = () => {
+  const input = readInputFile(path.join(__dirname, 'day03Input.txt'));
+
+  return input.map(line => {
+    let [id, x, y, dimensions] = line
+      .split(/\W/g)
+      .filter(str => str.length > 0);
+
+    [id, x, y] = [id, x, y].map(el => +el);
+
+    const [width, height] = dimensions.split('x').map(dims => +dims);
+
+    return {
+      id,
+      x,
+      y,
+      width,
+      height,
+    };
+  });
 };
+
+const problemA = input => {
+  const grid = {};
+  const rectangles = getRectangles();
+
+  rectangles.forEach(rect => {
+    const xCoords = Array.from({ length: rect.width }, (_, i) => i + rect.x);
+    const yCoords = Array.from({ length: rect.height }, (_, i) => i + rect.y);
+    const rectCoords = xCoords.map(xCoord => {
+      return yCoords.map(yCoord => {
+        return `${xCoord},${yCoord}`;
+      });
+    });
+
+    rectCoords.forEach(arr => {
+      arr.forEach(xyCoord => {
+        grid[xyCoord] = (grid[xyCoord] || 0) + 1;
+      });
+    });
+  });
+
+  return Object.values(grid).filter(count => count > 1).length;
+};
+
+console.log(problemA(getRectangles()));
 
 module.exports = problemA;
--- 2018/day03/partA.test.js
+++ 2018/day03/partA.test.js
@@ -1,6 +1,6 @@
 const problemA = require('./partA');
 
-xtest('Solves sample problem', () => {
-  const sampleInput = [];
-  expect(problemA(sampleInput)).toBe(44);
+test('Solves sample problem', () => {
+  const sampleInput = ['#1 @ 1,3: 4x4', '#2 @ 3,1: 4x4', '#3 @ 5,5: 2x2'];
+  expect(problemA(sampleInput)).toBe(4);
 });
